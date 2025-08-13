bf42791a70235fc48b1279ba49e8309692ea0856
sakshamsharma-code <nanus560.ns@gmail.com>
2025-08-13 23:14:39 +0530

    updated

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template, request
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import load_model # pyright: ignore[reportMissingImports]
+from tensorflow.keras.preprocessing import image # pyright: ignore[reportMissingImports]
 import numpy as np
 import os
 from werkzeug.utils import secure_filename
 from PIL import Image
 
 app = Flask(__name__)
-model = load_model('model.h5')  # Path to your trained Keras .h5 model
+model = load_model('model.h5')  # Path to your trained model
 
 @app.route('/')
 def home():
@@ -31,16 +31,16 @@ def predict():
             filepath = os.path.join('static', filename)
             file.save(filepath)
 
-            # Load & preprocess image
+            # Preprocess image
             img = Image.open(filepath).convert('RGB').resize((224, 224))
             img_array = image.img_to_array(img) / 255.0
             img_array = np.expand_dims(img_array, axis=0)
 
+            # Predict
             pred = model.predict(img_array)[0][0]
-            prediction = 'Cat 🐱' if pred > 0.5 else 'Dog 🐶'
+            prediction = 'Dog 🐶' if pred > 0.5 else 'Cat 🐱'
 
-
-            # remove uploaded image after prediction
+            # Remove uploaded file after prediction
             os.remove(filepath)
 
     return render_template('project.html', prediction=prediction)
